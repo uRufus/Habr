5375a9961f8448a30aac896ee04b2b2b5f82fcbb
Rufus <apdubrov@gmail.com>
2022-07-15 21:53:12 +0300

    Sprint 1

--- myHabr/myHabr/urls.py
+++ myHabr/myHabr/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mainapp.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', index, name='index'),
+    path('', index),
 ]
