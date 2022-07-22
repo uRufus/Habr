23f7d9648c08a6d1c821c6e03666887c52d66187
yura <yuverch@gmail.com>
2022-07-22 21:13:54 +0700

    создал BlogCategories, Blogs

--- myHabr/blogapp/models.py
+++ myHabr/blogapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from Habr.myHabr.mainapp.models import User
+
+
+class BlogCategories:
+    name = models.CharField(max_length=255, verbose_name="название")
+
+
+class Blogs(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    category = models.ForeignKey(BlogCategories, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=255, verbose_name="название")
