c9d6541f0cb57056c2373a98806cefcf6f284f5f
yura <yuverch@gmail.com>
2022-07-28 09:26:10 +0700

    вывод страниц

--- myHabr/blogapp/views.py
+++ myHabr/blogapp/views.py
@@ -2,17 +2,19 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .models import Blogs
+from mainapp.models import BlogPost
 
 
 def mainmyblogs(request):
-    postList = Blogs.objects.filter(user='1')
-    paginator = Paginator(postList, 3)
+    postList = BlogPost.objects.filter(author='1')
+    # postList = Blogs.objects.filter(user='1')
+    paginator = Paginator(postList, 1)
     page = request.GET.get('page')
     posts = paginator.get_page(page)
 
-    context = {
+    content = {
         "posts": posts,
         "title": "Мой блог",
         "desc": "Описание страницы",
     }
-    return render(request, "blogapp/index.html", context)
+    return render(request, "blogapp/index.html", content)
