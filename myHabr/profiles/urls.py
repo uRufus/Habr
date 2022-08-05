0701a6dd67613e2b920b6743d04f0d64d09aabae
karasevvs <karasev997@icloud.com>
2022-08-05 20:39:51 +0500

    корректировки профиля

--- myHabr/profiles/urls.py
+++ myHabr/profiles/urls.py
@@ -6,5 +6,6 @@ app_name = 'profiles'
 urlpatterns = [
     path('<int:id>/', views.profile, name='profile'),
     path('create_profile/<int:id>/', views.create_profile, name='create'),
-    path('create_update_profile/<int:id>/<str:action>/', views.update_profile, name='update')
+    path('create_update_profile/<int:id>/', views.update_profile, name='update')
+    # path('create_update_profile/<int:id>/<str:action>/', views.update_profile, name='update')
 ]
