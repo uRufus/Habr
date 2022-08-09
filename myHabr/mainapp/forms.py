c012f074451515b674bab2a75cf8e20b9fe51e40
Tititun <vinniopo@mail.ru>
2022-08-10 02:51:45 +0700

    исправил шаблоны и формы, чтобы включить тэги

--- myHabr/mainapp/forms.py
+++ myHabr/mainapp/forms.py
@@ -1,18 +1,21 @@
 from django import forms
-from .models import BlogPost
+from .models import BlogPost, Tag
 
 
 class BlogPostForm(forms.ModelForm):
     """Данная форма работает только с подключенным бутстрапом"""
     class Meta:
         model = BlogPost
-        fields = ('title', 'blog', 'body')
+        fields = ('title', 'blog', 'body', 'tags')
+        tags = forms.ModelMultipleChoiceField(
+            queryset=Tag.objects.all(),
+        )
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
             'blog': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
-
+            'tags': forms.SelectMultiple(attrs={'class': 'form-select'})
         }
 
