50bb59a578c5dbaa13cf56c8827193f6ef56266d
BandS-Y <yuverch@gmail.com>
2022-08-12 13:34:58 +0700

    Обновил и поправил реквест

--- myHabr/blogapp/models.py
+++ myHabr/blogapp/models.py
@@ -9,23 +9,24 @@ from authapp.models import MyHabrUser
 class BlogCategories(models.Model):
     name = models.CharField(max_length=255, verbose_name="название")
 
-    class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = 'Категории'
-
     def __str__(self):
         return self.name
 
 
 
 class Blogs(models.Model):
+    DELETED = "0"
+    PUBLISHED = '1'
+
+    BLOG_STATUS = (
+        (DELETED, 'блог удалена'),
+        (PUBLISHED, 'блог опубликован'),
+
+    )
+
     user = models.ForeignKey(MyHabrUser, on_delete=models.CASCADE)
     category = models.ForeignKey(BlogCategories, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, verbose_name="название")
-
-    class Meta:
-        verbose_name = "Блог"
-        verbose_name_plural = 'Блоги'
-
+    status = models.CharField(max_length=1, choices=BLOG_STATUS, default=PUBLISHED, verbose_name="статус блога")
     def __str__(self):
         return self.name
