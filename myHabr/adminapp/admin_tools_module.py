ec2cc13fec808066b31f8e45d1656370d6e7bf05
aaznutii <76644651+aaznutii@users.noreply.github.com>
2022-08-24 23:09:43 +0400

    Выполнено создание и первичное наполнение модуля для работы с обращениями

--- myHabr/adminapp/admin_tools_module.py
+++ myHabr/adminapp/admin_tools_module.py
@@ -1,24 +1,15 @@
 
 from admin_tools.dashboard import modules
-from mainapp.models import BlogPost, Comment
-from adminapp.admin import MessageAdmin
-from authapp.models import MyHabrUser
 
 
 class ToolsModule(modules.DashboardModule):
-
-    def is_empty(self):
-        return  ''
 
     def __init__(self, title, data):
         super(ToolsModule, self).__init__()
         self.template = '../templates/admin_tools_module.html'
         self.title = title
-        self.header_lst = ['Автор','Текст обращения','Тип обращения','Дата создания обращения', 'Активно']
+        self.header_lst = ['Автор', 'Текст обращения', 'Тип обращения', 'Дата создания обращения', 'Активно']
         self.data = data
 
-        # self.headers = list(data[0].keys())
-
-
-    # def init_with_context(self, context):
-    #     pass
+    def is_empty(self):
+        return ''
